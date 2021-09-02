bce7d6c953b61972eba978143eb6bc0523e3980d
Alan Sparrow <git@freeflight.org.uk>
2021-09-02 14:03:58 +0100

    Add GUI

---
+++ gui.py
@@ -0,0 +1,116 @@
+import json
+import sys
+
+import numpy as np
+import tkinter as tk
+
+import replay
+
+class App(tk.Frame):
+    def __init__(self, replay, master=None):
+        super().__init__(master)
+
+        self.replay = replay
+        self.log_len = replay.log_len
+
+        self._root().title("Replay")
+
+        self.pack(fill=tk.BOTH, expand=True)
+        self.create_widgets()
+
+        self.paused = False
+        self.drag = False
+
+        self.count = 0
+        self.tick()
+
+    def create_widgets(self):
+        self.scale = tk.Scale(self, from_=0, to=(self.log_len - 1),
+                orient=tk.HORIZONTAL, command=self.scale)
+        self.scale.showvalue = False
+        self.scale.bind('<Button-1>', self.button_press)
+        self.scale.bind('<ButtonRelease-1>', self.button_release)
+        self.scale.grid(row=0, column=0, columnspan=6, padx=8, pady=(8, 0),
+                sticky=tk.E+tk.W)
+
+        button = tk.Button(self, text='<<', command=self.transport(-10))
+        button.grid(row=1, column=0, padx=(8, 0), pady=8)
+
+        button = tk.Button(self, text='<', command=self.transport(-1),
+                repeatdelay=1000, repeatinterval=100)
+        button.grid(row=1, column=1, padx=(4, 0), pady=8)
+
+        self.pause_button_text = tk.StringVar()
+        button = tk.Button(self, text='Stop/Go', command=self.pause)
+        button.grid(row=1, column=2, padx=(8, 0), pady=8)
+
+        button = tk.Button(self, text='>', command=self.transport(1),
+                repeatdelay=1000, repeatinterval=100)
+        button.grid(row=1, column=3, padx=(8, 0), pady=8)
+
+        button = tk.Button(self, text='>>', command=self.transport(10))
+        button.grid(row=1, column=4, padx=(4, 0), pady=8)
+
+        button = tk.Button(self, text='Quit', command=self.master.destroy)
+        button.grid(row=1, column=5, padx=(8, 8), pady=8)
+
+    def transport(self, value):
+        def skip():
+            self.paused = True
+
+            self.count += value
+            if self.count < 0:
+                self.count = 0
+            elif self.count >= self.log_len:
+                self.count = self.log_len - 1
+        return skip
+
+    def scale(self, value):
+        if self.drag:
+            self.count = int(value)
+
+    def pause(self):
+        self.paused = not self.paused
+
+    def button_press(self, evt):
+        self.drag = True
+
+    def button_release(self, evt):
+        self.drag = False
+
+    def tick(self):
+        if self.drag or self.paused:
+            freeze = True
+        else:
+            if self.count < (self.log_len - 1):
+                self.count += 1
+            freeze = False
+
+        self.scale.set(self.count)
+
+        self.replay.replay(self.count, freeze=freeze)
+        self.after(100, self.tick)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'),
+            help='JSON data file')
+    parser.add_argument('id', nargs='?', help='id to replay')
+    parser.add_argument('--dist', '-d', type=int, default=0,
+            help='add if within distance (m)')
+    parser.add_argument('--port', '-p', type=int, default=5124,
+            help='FG port number')
+    parser.add_argument('--aircraft', '-a', choices=['dg101', 'asg29', 'spitfire', 'lego'],
+            default='asg29', help='aircraft model')
+    args = parser.parse_args()
+
+    data = json.load(args.file)
+    tdelta = data['tdelta']
+
+    replay_logs = replay.find_logs(args.id, data['logs'], args.dist)
+    replay = replay.Replay(replay_logs, tdelta, args.aircraft, args.port)
+
+    app = App(replay)
+    app.mainloop()
--- replay.py
+++ replay.py
@@ -6,64 +6,88 @@ import xdrlib
 
 import numpy as np
 
+class Replay:
+    def __init__(self, logs, tdelta, aircraft, port):
+        self.logs = logs
+        self.tdelta = tdelta
+        self.port = port
+
+        if aircraft == 'asg29':
+            self.model = 'Aircraft/ASG29/Models/asg29.xml'
+        elif aircraft == 'spitfire':
+            self.model = 'Aircraft/Spitfire/Models/spitfire_model.xml'
+        elif aircraft == 'lego':
+            self.model = 'Aircraft/ogel/Models/SinglePiston.xml'
+        else:
+            self.model = 'Aircraft/DG-101G/Models/DG-101G.xml'
+
+        self.log_len = len(logs[0]['data'])
+
+    def replay(self, n, freeze=False):
+        for log in self.logs:
+            x, y, z, orix, oriy, oriz, vx, vy, vz = log['data'][n]
+            if freeze:
+                vx, vy, vz = (0, 0, 0)
+
+            self.send_msg(log['id'], x, y, z, orix, oriy, oriz, vx, vy, vz)
+
+    def send_msg(self, id, x, y, z, orix, oriy, oriz, vx, vy, vz):
+        model = bytes(self.model, 'utf-8')
+        model += bytearray(96 - len(model))
+
+        # Not really sure what value this should be, but this seems to work
+        lag = self.tdelta
+
+        # Pack XDR data
+        packer = xdrlib.Packer()
+
+        utc_secs = time.time() % (24 * 3600)
+        packer.pack_double(utc_secs)
+        packer.pack_double(lag)
+
+        packer.pack_double(x)
+        packer.pack_double(y)
+        packer.pack_double(z)
+
+        packer.pack_float(orix)
+        packer.pack_float(oriy)
+        packer.pack_float(oriz)
+
+        packer.pack_float(vx)
+        packer.pack_float(vy)
+        packer.pack_float(vz)
+
+        # Angular velocity
+        packer.pack_float(0)
+        packer.pack_float(0)
+        packer.pack_float(0)
+
+        # Linear acceleration
+        packer.pack_float(0)
+        packer.pack_float(0)
+        packer.pack_float(0)
+
+        # Angular acceleration
+        packer.pack_float(0)
+        packer.pack_float(0)
+        packer.pack_float(0)
+
+        data = model + packer.get_buffer() + bytearray(4)
+
+        # Make header
+        idb = bytes(id[:8], 'ascii')
+        hdr = b"FGFS" + \
+              bytearray([0, 1, 0, 1,
+                         0, 0, 0, 7,
+                         0, 0, 0, len(data)+32,
+                         0, 0, 0, 0,
+                         0, 0, 0, 0]) + idb + bytearray(8 - len(idb))
+        msg = hdr + data
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(msg, ("127.0.0.1", self.port))
+
 #----------------------------------------------------------------------
-
-def send_msg(port, id, x, y, z, orix, oriy, oriz, vx, vy, vz, aircraft):
-    model = bytes(aircraft, 'utf-8')
-    model += bytearray(96 - len(model))
-
-    # Not sure what value this should be, but less than this gives jerky
-    # playback
-    lag = 0.1
-
-    # Pack XDR data
-    packer = xdrlib.Packer()
-
-    utc_secs = time.time() % (24 * 3600)
-    packer.pack_double(utc_secs)
-    packer.pack_double(lag)
-
-    packer.pack_double(x)
-    packer.pack_double(y)
-    packer.pack_double(z)
-
-    packer.pack_float(orix)
-    packer.pack_float(oriy)
-    packer.pack_float(oriz)
-
-    packer.pack_float(vx)
-    packer.pack_float(vy)
-    packer.pack_float(vz)
-
-    # Angular velocity
-    packer.pack_float(0)
-    packer.pack_float(0)
-    packer.pack_float(0)
-
-    # Linear acceleration
-    packer.pack_float(0)
-    packer.pack_float(0)
-    packer.pack_float(0)
-
-    # Angular acceleration
-    packer.pack_float(0)
-    packer.pack_float(0)
-    packer.pack_float(0)
-
-    data = model + packer.get_buffer() + bytearray(4)
-
-    # Make header
-    idb = bytes(id[:8], 'ascii')
-    hdr = b"FGFS" + \
-          bytearray([0, 1, 0, 1,
-                     0, 0, 0, 7,
-                     0, 0, 0, len(data)+32,
-                     0, 0, 0, 0,
-                     0, 0, 0, 0]) + idb + bytearray(8 - len(idb))
-    msg = hdr + data
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(msg, ("127.0.0.1", port))
 
 # Calculate minimum distance between two logs
 def min_distance(data1, data2):
@@ -74,16 +98,7 @@ def min_distance(data1, data2):
     return np.min(dist)
 
 # Replay log data
-def replay(id, logs, tdelta, dist, port, aircraft):
-    if aircraft == 'asg29':
-        model = 'Aircraft/ASG29/Models/asg29.xml'
-    elif aircraft == 'spitfire':
-        model = 'Aircraft/Spitfire/Models/spitfire_model.xml'
-    elif aircraft == 'lego':
-        model = 'Aircraft/ogel/Models/SinglePiston.xml'
-    else:
-        model = 'Aircraft/DG-101G/Models/DG-101G.xml'
-
+def find_logs(id, logs, dist):
     # Find log data
     ref_log = None
     for log in logs:
@@ -106,13 +121,7 @@ def replay(id, logs, tdelta, dist, port, aircraft):
                 print("Adding log " + log_id + ", min distance %d" % mdist)
                 replay_logs.append(log)
 
-    # Send data to FG sim
-    for i in range(len(ref_log['data'])):
-        for log in replay_logs:
-            x, y, z, orix, oriy, oriz, vx, vy, vz = log['data'][i]
-            send_msg(port, log['id'], x, y, z, orix, oriy, oriz, vx, vy, vz, model)
-
-        time.sleep(tdelta)
+    return replay_logs
 
 if __name__ == '__main__':
     import argparse
@@ -129,7 +138,6 @@ if __name__ == '__main__':
             help='print list of IDs in file')
     parser.add_argument('--aircraft', '-a', choices=['dg101', 'asg29', 'spitfire', 'lego'],
             default='asg29', help='aircraft model')
-
     args = parser.parse_args()
 
     data = json.load(args.file)
@@ -137,4 +145,11 @@ if __name__ == '__main__':
         print(", ".join(data['ids']))
         sys.exit(0)
 
-    replay(args.id, data['logs'], data['tdelta'], args.dist, args.port, args.aircraft)
+    tdelta = data['tdelta']
+
+    replay_logs = find_logs(args.id, data['logs'], args.dist)
+
+    replay = Replay(replay_logs, tdelta, args.aircraft, args.port)
+    for i in range(replay.log_size):
+        replay.replay(i)
+        time.sleep(tdelta)
